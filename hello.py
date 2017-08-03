b3440e13be2e424ad6d5991ebbe94f4a14732d92
jackyboi <z5163071@student.unsw.edu.au>
2017-08-03 11:10:09 +1000

    Added FizzBuzz function

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("Hello, git!")
+
+def FizzBuzz(n):
+        for i in range(1, 101):
+                    print(('' if i%3 else 'Fizz')+('' if i%5 else 'Buzz') or i)
+
+n = int(input("Enter number: "))
+FizzBuzz(n)
+
