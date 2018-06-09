3a60b0bbcbe0af362a1fc25549a035b834313e1e
Jack Gardner <z5163071@student.unsw.edu.au>
2018-06-09 17:43:54 +1000

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,1 @@
-def FizzBuzz(n):
-        print('\n'.join([('' if i%3 else 'Fizz')+('' if i%5 else 'Buzz') or str(i) for i in range(1, n)]))
-
-n = int(input("Enter a number: "))
-FizzBuzz(n)
+for i in range(1,101): print((''if i%3 else'Fizz')+(''if i%5 else'Buzz')or i)
